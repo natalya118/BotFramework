50621cac0bd31b92d73dd99f74c2dc09c5e949ec
Nataliia Dranchuk <nat.dranchuk@gmail.com>
2018-05-04 16:54:12 +0300

    Simple Bot added

---
+++ SimpleBot/SimpleBot/Dialogs/RootDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace SimpleBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync("Hi! What's your name?");
+            context.Wait(NameReceivedAsync);
+        }
+
+        private async Task NameReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            if (activity != null)
+                await context.PostAsync($"Welcome, {activity.Text}");
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
